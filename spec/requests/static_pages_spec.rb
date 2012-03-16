f880d1f06f39c6c41bc62ebff3f70d9c77ebf849
Masahiro Hayashi <tmasahirot@hotmail.co.jp>
2012-03-16 19:34:05 +0900

    exercise5-6-2

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -41,4 +41,18 @@ describe "Static Pages" do
 
     it_should_behave_like "all static pages"
   end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact')
+    click_link "Home"
+    page.should have_selector 'title', text: full_title('')
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title('Sign up')
+  end
 end
